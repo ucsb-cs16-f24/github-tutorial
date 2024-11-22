f75d71fa173beb5f284af39a2042ebf5a8ed7732
Erik <41846897+Phangzs@users.noreply.github.com>
2024-11-21 17:03:28 -0800

    Initial commit

---
+++ calculator.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    char operation;
+    double num1, num2, result;
+
+    cout << "Simple Calculator" << endl;
+    cout << "Enter an operation (+, -, *, /): ";
+    cin >> operation;
+
+    cout << "Enter the first number: ";
+    cin >> num1;
+
+    cout << "Enter the second number: ";
+    cin >> num2;
+
+    switch (operation) {
+        case '+':
+            result = num1 + num2;
+            cout << "Result: " << result << endl;
+            break;
+        case '-':
+            result = num1 - num2;
+            cout << "Result: " << result << endl;
+            break;
+        case '*':
+            result = num1 * num2;
+            cout << "Result: " << result << endl;
+            break;
+        case '/':
+	    if (num2 == 0) {cerr << "Error: Division by 0!" << endl; exit(1);}
+            result = num1 / num2;  // No check for dividing by 0
+            cout << "Result: " << result << endl;
+            break;
+        default:
+            cout << "Invalid operation!" << endl;
+            break;
+    }
+
+    return 0;
+}
+
+
